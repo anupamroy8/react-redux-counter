87ca6cb18ab041363aa29ae77bfd3cf90a10b8a1
anupamroy8 <anupamroy8@gmail.com>
2020-07-17 18:19:54 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from 'redux';
+import { store } from "./store/index";
 import "./index.css";
 import App from "./App";
-const store = createStore();
-
 
 ReactDOM.render(
   <React.StrictMode>
