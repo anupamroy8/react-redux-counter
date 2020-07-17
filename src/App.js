97c82f62c1bfe3a97510193f7e693069fdd26297
anupamroy8 <anupamroy8@gmail.com>
2020-07-17 09:49:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>1</h1>
+      <button className="inc">Increment</button>
+      <button className="dec">Decrement</button>
+      <button className="reset">Reset</button>
+    </div>
+  );
+}
+
+export default App;
